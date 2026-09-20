4d0a0ba10c4b5dfa5b7f773c2d95c3102db2ce5d
Oleksandr Overchuk <alek.owerczuk@gmail.com>
2026-09-20 16:20:51 +0200

    Reapply "some fixes + image +"
    This reverts commit 1becd00deba6fa0b248996a2c0ce5e1c201bf613.

--- backend/src/main/java/pl/jollycart/image/OfferImageRepository.java
+++ backend/src/main/java/pl/jollycart/image/OfferImageRepository.java
@@ -1,5 +1,11 @@
 package pl.jollycart.image;
 
-public class OfferImageRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfferImageRepository
+        extends JpaRepository<OfferImage, Long> {
+
+    List<OfferImage> findByOfferIdOrderByCreatedAtAsc(Long offerId);
 }
